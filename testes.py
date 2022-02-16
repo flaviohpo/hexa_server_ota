28ece13756a34c5a8d71734c55683c753f3f1927
flaviohpo <flaviohpo@yahoo.com>
2022-02-15 22:01:24 -0300

    Ajustado o exemplo do blink, removido codigo desnecessario

--- testes.py
+++ testes.py
@@ -3,3 +3,14 @@ with open('blink.bin', 'rb') as firm:
     while b:
         print(f'0x'+f'{bytes(b).hex()}'.upper(), end=' ')
         b = firm.read(1)
+
+
+
+# inicio do trecho python
+lista_apostilas = ['HTML, CSS e Javascript', 'Java para Web']
+
+@app.route('/apostilas')
+def apostilas_online():
+    return render_template('apostilas.html', lista=lista_apostilas)
+
+# fim trecho
