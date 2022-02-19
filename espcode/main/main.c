83979025febbcc80deba9dc5716b0fa12d04292e
flaviohpo <flaviohpo@yahoo.com>
2022-02-18 21:30:57 -0300

    http request funcionando

--- espcode/main/main.c
+++ espcode/main/main.c
@@ -1,37 +1,294 @@
-#include <stdio.h>
+/* 
+pinos utilizados
+TXD2 20
+RXD2 19
+SET  8
+RTS2 41
+CTS2 42
+*/
+#include <string.h>
+#include "stdio.h"
+#include "stdlib.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+
 #include "driver/gpio.h"
+#include "driver/uart.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
 #include "esp_log.h"
-#include "sdkconfig.h"
+#include "esp_http_client.h"
 
-#define BLINK_GPIO CONFIG_BLINK_GPIO
-static const char *TAG = "example";
-static uint8_t s_led_state = 0;
+#include "nvs_flash.h"
 
-static void blink_led(void)
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+/* The examples use WiFi configuration that you can set via project configuration menu
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID      "MaisUmaRede"
+#define EXAMPLE_ESP_WIFI_PASS      "m1m1uK1___"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  3
+
+#define BLINK_GPIO      48
+#define HC12_SET_GPIO   8
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
 {
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
 }
 
-static void configure_led(void)
+void wifi_init_sta(void)
 {
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = 
+    {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	        .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = 
+            {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+
+}
+
+
+esp_err_t client_event_handler(esp_http_client_event_t *evt)
+{
+   //esp_http_client_event_id_t event_id;    /*!< event_id, to know the cause of the event */
+   //esp_http_client_handle_t client;        /*!< esp_http_client_handle_t context */
+   //void *data;                             /*!< data of the event */
+   //int data_len;                           /*!< data length of data */
+   //void *user_data;                        /*!< user_data context, from esp_http_client_config_t user_data */
+   //char *header_key;                       /*!< For HTTP_EVENT_ON_HEADER event_id, it's store current http header key */
+   //char *header_value;     
+
+    switch (evt->event_id)
+    {
+        case HTTP_EVENT_ERROR:
+            printf("HTTP_EVENT_ERROR\n");
+        break;
+
+        case HTTP_EVENT_ON_CONNECTED:
+            printf("HTTP_EVENT_ON_CONNECTED\n");
+        break;
+
+        case HTTP_EVENT_HEADERS_SENT:
+            printf("HTTP_EVENT_HEADERS_SENT\n");
+        break;
+
+        case HTTP_EVENT_ON_HEADER:
+            printf("HTTP_EVENT_ON_HEADER: key=%s value=%s\n", evt->header_key, evt->header_value);
+        break;
+
+        case HTTP_EVENT_ON_DATA:
+            printf("HTTP_EVENT_ON_DATA: %s\n", (char*)evt->data);
+        break;
+
+        case HTTP_EVENT_ON_FINISH:
+            printf("HTTP_EVENT_ON_FINISH\n");
+        break;
+
+        case HTTP_EVENT_DISCONNECTED:
+            printf("HTTP_EVENT_DISCONNECTED\n");
+        break;
+
+        default:
+            printf("EVENTO HTTP NAO TRATADO: %d\n", evt->event_id);
+        break;
+    }
+
+    return ESP_OK;
+}
+
+
+void get_flask_api(void)
+{
+    esp_http_client_config_t client_config = 
+    {
+        .url = "http://866d-2804-7f5-9392-c8f9-2a98-1385-a5f2-56da.ngrok.io/",
+        .event_handler = client_event_handler
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&client_config);
+    esp_http_client_perform(client);
+    esp_http_client_cleanup(client);
+}
+
+void get_worldclock_api(void)
+{
+    esp_http_client_config_t client_config = 
+    {
+        .url = "http://worldclockapi.com/api/json/utc/now",
+        .event_handler = client_event_handler
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&client_config);
+    esp_http_client_perform(client);
+    esp_http_client_cleanup(client);
 }
 
 void app_main(void)
 {
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-    /* Configure the peripheral according to the LED type */
-    configure_led();
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
 
-    while (1) {
-        blink_led();
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    get_worldclock_api();
+    //get_flask_api();
+
+    const uart_port_t uart_num = UART_NUM_2;
+    uart_config_t uart_config = {
+        .baud_rate = 2400,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    // hc12 set pin
+    gpio_pad_select_gpio(HC12_SET_GPIO);
+    gpio_set_direction(HC12_SET_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(HC12_SET_GPIO, 0);
+    // Configure UART parameters
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 20, 19, 41, 42));
+    // Setup UART buffered IO with event queue
+    const int uart_buffer_size = (1024 * 2);
+    QueueHandle_t uart_queue;
+    // Install UART driver using an event queue here
+    ESP_ERROR_CHECK(uart_driver_install(    UART_NUM_2, 
+                                            uart_buffer_size, 
+                                            uart_buffer_size, 
+                                            10, 
+                                            &uart_queue, 
+                                            0));
+    // Write data to UART.
+    char* test_str = "AT+RX\n\r";
+    uart_write_bytes(uart_num, (const char*)test_str, strlen(test_str));
+    printf("\n%s\n", test_str);
+    vTaskDelay(100);
+    // Read data from UART.
+    uint8_t data[128] = {0};
+    int length = 0;
+    ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t*)&length));
+    length = uart_read_bytes(uart_num, data, length, 100);
+    printf("\n%s\n", (char*)data);
+
+    gpio_pad_select_gpio(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while(1)
+    {
+
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(100);
+
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(100);
     }
 }
